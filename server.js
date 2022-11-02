b028c5141eac64df5129951daa7e08d9e22a6da8
PRamsey02 <prestonkramsey02@icloud.com>
2022-11-02 00:47:39 -0500

    Added update function for employee, manager | Function to view employee by department

--- server.js
+++ server.js
@@ -25,27 +25,6 @@ afterConnection = () => {
     promptUser();
   };
 
-// Wasn't able to get the clean Employee Manager image :(
-// afterConnection = () => {
-//     console.log(",-----------------------------------------------------.")
-//     console.log("|                                                     |")
-//     console.log("|    _____                 _                          |")
-//     console.log("|   | ____|_ __ ___  _ __ | | ___  _   _  ___  ___    |")
-//     console.log("|   |  _| | '_ ` _ \| '_ \| |/ _ \| | | |/ _ \/ _ \   |")
-//     console.log("|   | |___| | | | | | |_) | | (_) | |_| |  __/  __/   |")
-//     console.log("|   |_____|_| |_| |_| .__/|_|\___/ \__, |\___|\___|   |")
-//     console.log("|                   |_|            |___/              |")
-//     console.log("|    __  __                                           |")
-//     console.log("|   |  \/  | __ _ _ __   __ _  __ _  ___ _ __         |")
-//     console.log("|   | |\/| |/ _ `| '_ \ / _ `|/ _ `|/ _ \ '__|        |")
-//     console.log("|   | |  | | (_| | | | | (_| | (_| |  __/ |           |")
-//     console.log("|   |_|  |_|\__,_| | |_|\__,_|\__, |\___|_|           |")
-//     console.log("|                             |___/                   |")
-//     console.log("|                                                     |")
-//     console.log("`-----------------------------------------------------'")
-//     promptUser();
-//   };
-
 // Inquirer prompt for user questions
 const promptUser = () => {
     inquirer.prompt ([
@@ -349,4 +328,141 @@ addEmployee = () => {
           });
        });
     });
+  };
+
+  // Function to update an employee
+  updateEmployee = () => {
+    // Require employees from employee table
+    const employeeSql = `SELECT * FROM employee`;
+  
+    connection.promise().query(employeeSql, (err, data) => {
+      if (err) throw err; 
+  
+    const employees = data.map(({ id, first_name, last_name }) => ({ name: first_name + " "+ last_name, value: id }));
+  
+      inquirer.prompt([
+        {
+          type: 'list',
+          name: 'name',
+          message: "Which employee would you like to update?",
+          choices: employees
+        }
+      ])
+        .then(empChoice => {
+          const employee = empChoice.name;
+          const params = []; 
+          params.push(employee);
+  
+          const roleSql = `SELECT * FROM role`;
+  
+          connection.promise().query(roleSql, (err, data) => {
+            if (err) throw err; 
+  
+            const roles = data.map(({ id, title }) => ({ name: title, value: id }));
+            
+              inquirer.prompt([
+                {
+                  type: 'list',
+                  name: 'role',
+                  message: "What is the employee's new role?",
+                  choices: roles
+                }
+              ])
+                  .then(roleChoice => {
+                  const role = roleChoice.role;
+                  params.push(role); 
+                  
+                  let employee = params[0]
+                  params[0] = role
+                  params[1] = employee 
+  
+                  const sql = `UPDATE employee SET role_id = ? WHERE id = ?`;
+  
+                  connection.query(sql, params, (err, result) => {
+                    if (err) throw err;
+                  console.log("Employee has been updated!");
+                
+                  showEmployees();
+            });
+          });
+        });
+      });
+    });
+  };
+  
+  // Function to update a manager
+  updateManager = () => {
+    // Require employees from employee table
+    const employeeSql = `SELECT * FROM employee`;
+  
+    connection.promise().query(employeeSql, (err, data) => {
+      if (err) throw err; 
+  
+    const employees = data.map(({ id, first_name, last_name }) => ({ name: first_name + " "+ last_name, value: id }));
+  
+      inquirer.prompt([
+        {
+          type: 'list',
+          name: 'name',
+          message: "Which employee would you like to update?",
+          choices: employees
+        }
+      ])
+        .then(empChoice => {
+          const employee = empChoice.name;
+          const params = []; 
+          params.push(employee);
+  
+          const managerSql = `SELECT * FROM employee`;
+  
+            connection.promise().query(managerSql, (err, data) => {
+              if (err) throw err; 
+  
+            const managers = data.map(({ id, first_name, last_name }) => ({ name: first_name + " "+ last_name, value: id }));
+              
+                inquirer.prompt([
+                  {
+                    type: 'list',
+                    name: 'manager',
+                    message: "Who is the employee's manager?",
+                    choices: managers
+                  }
+                ])
+                    .then(managerChoice => {
+                      const manager = managerChoice.manager;
+                      params.push(manager); 
+                      
+                      let employee = params[0]
+                      params[0] = manager
+                      params[1] = employee
+
+                      const sql = `UPDATE employee SET manager_id = ? WHERE id = ?`;
+
+                      connection.query(sql, params, (err, result) => {
+                        if (err) throw err;
+                      console.log("Employee has been updated!");
+                    
+                      showEmployees();
+            });
+          });
+        });
+      });
+    });
+  };
+  
+  // Function to view employee by department
+  employeeDepartment = () => {
+    console.log('Showing employee by departments...\n');
+    const sql = `SELECT employee.first_name, 
+                        employee.last_name, 
+                        department.name AS department
+                 FROM employee 
+                 LEFT JOIN role ON employee.role_id = role.id 
+                 LEFT JOIN department ON role.department_id = department.id`;
+  
+    connection.promise().query(sql, (err, rows) => {
+      if (err) throw err; 
+      console.table(rows); 
+      promptUser();
+    });          
   };
